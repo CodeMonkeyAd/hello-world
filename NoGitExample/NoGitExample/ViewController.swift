6a1de2bb606802e7f8e1de9d3ebbb3dd14fb6f84
CodeMonkeyAd <447696672@qq.com>
2017-06-05 16:19:25 +0800

    Add files via upload

---
+++ NoGitExample/NoGitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoGitExample
+//
+//  Created by Andylau on 2017/6/5.
+//  Copyright © 2017年 andylau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
